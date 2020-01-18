52d2261de5b0936ec58f97e78112185e10cf48d4
ajraj27 <rajanuj.iiita@gmail.com>
2020-01-18 22:37:30 +0530

    update post and comment

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -118,6 +118,28 @@ const Mutation = {
         return deletedPosts[0]
     },
 
+    updatePost(parent,args,{db},info){
+        const post=db.posts.find((post) => post.id===args.id)
+
+        if(!post){
+            throw new Error("Post not found")
+        }
+        
+        if(typeof args.data.title==="string"){
+            post.title=args.data.title
+        }
+
+        if(typeof args.data.body==="string"){
+            post.body=args.data.body
+        }
+
+        if(typeof args.data.published==="boolean"){
+            post.published=args.data.published
+        }
+
+        return post
+    },
+
     deleteComment(parent,args,{db},info){
         const commentIndex=db.comments.findIndex((comment) => comment.id===args.id)
 
@@ -127,6 +149,18 @@ const Mutation = {
         const deletedComments=db.comments.splice(commentIndex,1)
         
         return deletedComments[0]
+    },
+
+    updateComment(parent,args,{db},info){
+        const comment=db.comments.find((comment) => comment.id===args.id)
+
+        if(!comment)
+            throw new Error("Comment not found.")
+
+        if(typeof args.data.text==="string")
+            comment.text=args.data.text
+            
+        return comment    
     }
 }
 

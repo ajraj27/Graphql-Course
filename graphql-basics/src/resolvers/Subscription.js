c33142bd9a8898660e511c76d02529ca426f2493
ajraj27 <rajanuj.iiita@gmail.com>
2020-01-21 03:47:13 +0530

    added comment subscription

--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -14,6 +14,14 @@ const Subscription={
 
             return pubsub.asyncIterator('count')
         }
+    },
+    comment:{
+        subscribe(parent,{postID},{db,pubsub},info){
+            const post=db.posts.find((post) => post.id===postID && post.published)
+            if(!post) throw new Error('Post not found.')
+
+            return pubsub.asyncIterator(`comment ${postID}`)
+        }
     }
 }
 
